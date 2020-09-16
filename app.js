968c829c18330343f86a9b3bfe7e7b0238fa04b4
fsdr <filipsordyl8@unilever.com>
2020-09-16 23:25:19 +0200

    COPYING TEXT

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-document.querySelector("form").addEventListener('submit',(event)=>{
-    event.preventDefault()
 
-    const input = document.querySelector('#encrypt')
-    console.log(input.value)
-})
 const input = document.querySelector('#encrypt');
 const text = document.querySelector('#code');
 const link = document.querySelector("#link");
+const copy = document.querySelector('.copy');
+document.querySelector("form").addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const share = document.querySelector('.share');
+    link.value = `${window.location}#${btoa(input.value)}`
+    copier.innerText = 'COPY!'
+    share.classList.remove('hide')
+    
+})
 
 input.addEventListener('input',(event) => {
     text.value =btoa(event.target.value);
     
 });
+
+function copyMe(){
+    copy.select()
+    copy.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
+const copier = document.querySelector(".copier")
+copier.addEventListener('click',(event) =>{
+    copyMe();
+    event.target.innerText = "Copied!"
+    
+})
