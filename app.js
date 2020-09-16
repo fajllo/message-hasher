fa53d6bfe208d4a1c1284e7aab05f330525811df
fsdr <filipsordyl8@unilever.com>
2020-09-16 22:57:46 +0200

    mor html and js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 document.querySelector("form").addEventListener('submit',(event)=>{
     event.preventDefault()
 
-    const input = document.querySelector('input')
+    const input = document.querySelector('#encrypt')
     console.log(input.value)
 })
+const input = document.querySelector('#encrypt');
+const text = document.querySelector('#code');
+const link = document.querySelector("#link");
+
+input.addEventListener('input',(event) => {
+    text.value =btoa(event.target.value);
+    
+});
