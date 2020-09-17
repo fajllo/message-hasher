8c19bd692fe633616547c54aaa9099c2c3a36772
fsdr <filipsordyl8@unilever.com>
2020-09-17 21:48:19 +0200

    finishe product

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const input = document.querySelector('#encrypt');
 const text = document.querySelector('#code');
 const link = document.querySelector("#link");
 const copy = document.querySelector('.copy');
+const massage =document.querySelector("#massage");
+const encode = document.querySelector(".encode");
+const show = document.querySelector(".show");
 document.querySelector("form").addEventListener('submit',(event)=>{
     event.preventDefault()
     const share = document.querySelector('.share');
@@ -13,7 +16,8 @@ document.querySelector("form").addEventListener('submit',(event)=>{
 })
 
 input.addEventListener('input',(event) => {
-    text.value =btoa(event.target.value);
+    text.value = btoa(event.target.value);
+    massage.classList.remove("hide")
     
 });
 
@@ -28,3 +32,10 @@ copier.addEventListener('click',(event) =>{
     event.target.innerText = "Copied!"
     
 })
+const hash = window.location.hash.split("#");
+console.log(hash[hash.length -1])
+if(hash[hash.length -1]){
+    encode.classList.remove("hide")
+    show.value = atob(hash[hash.length -1])
+
+}
